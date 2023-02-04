5cf695a89a533ea2e3b247fc055afa1d8aaa8861
Brian Guarraci <brian@ops5.com>
2023-02-04 11:02:42 -0700

    add klongpy example

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ KlongPy enabled DFS service
 * DataFrame service that can run KlongPy on DataFrames
 * Client REPL can send KlongPy operations to remote service 
 
+# Quick start
+
+## Start Server
+
+```bash
+$ ks_server --info
+```
+
+## Run REPL
+
+```bash
+$ ks_repl
+Creating connection pool with 8 connections
+
+Welcome to KS REPL
+author: Brian Guarraci
+repo  : https://github.com/briangu/kfs
+crtl-c to quit
+
+?> ! 1+1
+{'result': '2'}
+```
--- ks/ks_server.py
+++ ks/ks_server.py
@@ -6,6 +6,7 @@ import simdjson as json
 
 from dfs.helpers import *
 from dfs import df_server
+from klongpy import KlongInterpreter
 
 
 class CommandHandler(df_server.CommandHandler):
@@ -15,12 +16,16 @@ class CommandHandler(df_server.CommandHandler):
         should_continue = True
         if command['type'] == 'klongpy':
             print(command)
-            send_success(conn)
+            send_json(conn, result=str(self.server.klong(command['lambda'])))
         else:
             should_continue = super().process_command(conn, command)
         return should_continue
 
 
 class KsServer(df_server.DataFrameServer):
-    pass
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.klong = KlongInterpreter()
+
 
