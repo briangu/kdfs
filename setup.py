388edb0edeb4850255da90e5c99886123bc524d2
Brian Guarraci <brian@ops5.com>
2023-02-04 09:22:53 -0700

    add pypi cofnig

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+
+setup(
+    name='klongpy_dfs_service',
+    packages=['ks'],
+    version='0.1.0',
+    description='DataFrame service with embedded KlongPy.',
+    author='Brian Guarraci',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=['colorama', 'klongpy', 'dataframe-service'],
+    python_requires='>=3.8',
+    include_package_data=True,
+    test_suite='tests',
+    scripts=['scripts/ks_server', 'scripts/ks_repl']
+)
