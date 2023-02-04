cb7bdf9936f172cbe592188feadb5b8b9e5b9490
Brian Guarraci <brian@ops5.com>
2023-02-04 10:30:48 -0700

    start wiring up KlongPy

--- ks/ks_client.py
+++ ks/ks_client.py
@@ -15,30 +15,7 @@ class KsClient(df_client.DataFrameClient):
         self.pool = pool
         self.conn = conn
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.pool.release_connection(self.conn)
-
-    def get_data(self, *args, range_start=None, range_end=None, range_type="timestamp"):
-        send_cmd(self.conn, 'get', file_path=args, range_start=range_start, range_end=range_end, range_type=range_type)
-        return recv_df(self.conn)
-
-    def insert_data(self, df, *args):
-        send_cmd(self.conn, 'insert', file_path=args)
-        send_df(self.conn, df)
-        recv_status(self.conn)
-
-    def unload(self, *args):
-        send_cmd(self.conn, 'unload', file_path=args)
-        recv_status(self.conn)
-
-    def load(self, *args):
-        send_cmd(self.conn, 'load', file_path=args)
-        return recv_json(self.conn)
-
-    def get_stats(self, level=None):
-        send_msg(self.conn, json.dumps({'type': 'stats', 'level': level}).encode())
+    def remote_klongpy(self, klong):
+        send_msg(self.conn, json.dumps({'type': 'klongpy', 'lambda': klong}).encode())
         return recv_json(self.conn)
 
